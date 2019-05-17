c7487e8ab90682d0b538501142c401523e00b8f8
Max Sun <max.sun@yale.edu>
2019-05-17 10:24:02 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,8 +4,7 @@
 def key_for_min_value(name_hash)
   if name_hash != {}
     name_hash.each do |key, value|
-      if key < lowest[0]
-        lowest.unshift(key)
+      if
       end
     end
   end
