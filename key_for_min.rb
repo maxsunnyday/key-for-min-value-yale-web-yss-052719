00095748ef5b311e24f5b2ce580eb3e657abd183
Max Sun <max.sun@yale.edu>
2019-05-17 11:03:05 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,14 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  lowest = float::INFINITY
   if name_hash != {}
     name_hash.each do |key, value|
-
+      if value < lowest
+        lowest = v
+        lowest_key = key
+      end
     end
+    lowest_key
   end
 end
